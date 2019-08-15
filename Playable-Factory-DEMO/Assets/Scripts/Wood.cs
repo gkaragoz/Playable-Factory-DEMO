a3606040185a0c2b7051723dcc9c22e8da0a3f10
gkaragoz <gkaragoz96@gmail.com>
2019-08-15 16:04:50 +0300

    Create wood.cs script and implement half of destruction feature.

---
+++ Playable-Factory-DEMO/Assets/Scripts/Wood.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Wood : MonoBehaviour {
+
+    [SerializeField]
+    private Side _side;
+
+    private MeshCollider _collider = null;
+    private Rigidbody _rigidbody = null;
+
+    public enum Side {
+        Right,
+        Left
+    }
+
+    private void AddCollider() {
+        _collider = this.gameObject.AddComponent<MeshCollider>();
+        _collider.convex = true;
+    }
+
+    private void IgnoreCollision(BoxCollider other) {
+        Physics.IgnoreCollision(_collider, other);
+    }
+
+    private void AddRigidbody() {
+        _rigidbody = this.gameObject.AddComponent<Rigidbody>();
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+    }
+
+    public void OnDestructed(BoxCollider ignoredCollision) {
+        AddCollider();
+        IgnoreCollision(ignoredCollision);
+        AddRigidbody();
+
+        _rigidbody.velocity = Vector3.forward;
+        _rigidbody.AddForceAtPosition(Vector3.forward * Random.Range(10, 80), transform.position);
+    }
+
+}
--- Playable-Factory-DEMO/Assets/Scripts/WoodDestructor.cs
+++ Playable-Factory-DEMO/Assets/Scripts/WoodDestructor.cs
@@ -2,30 +2,24 @@
 
 public class WoodDestructor : MonoBehaviour {
 
-    [Header("Debug Only")]
+    [Header("Initializations")]
     [SerializeField]
-    private bool _hasDestructed = false;
-    [SerializeField]
-    private Transform _woodLeftTransform = null;
-    [SerializeField]
-    private Transform _woodRightTransform = null;
+    private Wood[] _woodPieces = null;
     [SerializeField]
     private CapsuleCollider _mainCollider = null;
     [SerializeField]
     private Rigidbody _rigidbody = null;
+
+    [Header("Debug Only")]
+    [SerializeField]
+    private bool _hasDestructed = false;
     [SerializeField]
     private BoxCollider _bladeCollider = null;
 
     private void Awake() {
         _bladeCollider = GameObject.FindGameObjectWithTag("Blade").GetComponent<BoxCollider>();
 
-        _woodLeftTransform = transform.GetChild(0);
-        _woodRightTransform = transform.GetChild(1);
-
-        _mainCollider = transform.GetComponent<CapsuleCollider>();
-        _rigidbody = GetComponent<Rigidbody>();
-
-        if (_woodLeftTransform == null || _woodRightTransform == null || _mainCollider == null || _rigidbody == null) {
+        if (_woodPieces == null || _mainCollider == null || _rigidbody == null) {
             Debug.LogError("Initialization failed. Check assignments of the Wood parts.");
         }
     }
@@ -48,38 +42,17 @@ public class WoodDestructor : MonoBehaviour {
         RemoveMainRigidbody();
         RemoveMainCollider();
 
-        AddCollidersToPieces();
-        AddRigidbodiesToPieces();
+        foreach (Wood wood in _woodPieces) {
+            wood.OnDestructed(_bladeCollider);
+        }
     }
 
     private void RemoveMainCollider() {
-        Destroy(_mainCollider);
+        _mainCollider.enabled = false;
     }
 
     private void RemoveMainRigidbody() {
-        Destroy(_rigidbody);
-    }
-
-    private void AddCollidersToPieces() {
-        MeshCollider colliderLeft = _woodLeftTransform.gameObject.AddComponent<MeshCollider>();
-        colliderLeft.convex = true;
-        Physics.IgnoreCollision(colliderLeft, _bladeCollider);
-
-        MeshCollider colliderRight = _woodRightTransform.gameObject.AddComponent<MeshCollider>();
-        colliderRight.convex = true;
-        Physics.IgnoreCollision(colliderRight, _bladeCollider);
-    }
-
-    private void AddRigidbodiesToPieces() {
-        Rigidbody _rbLeft = _woodLeftTransform.gameObject.AddComponent<Rigidbody>();
-        Rigidbody _rbRight = _woodRightTransform.gameObject.AddComponent<Rigidbody>();
-
-        _rbLeft.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-        _rbLeft.velocity = -1 * Vector3.forward;
-        _rbLeft.AddForceAtPosition(Vector3.forward * Random.Range(10, 80), transform.position);
-        _rbRight.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-        _rbRight.velocity = -1 * Vector3.forward;
-        _rbRight.AddForceAtPosition(Vector3.forward * Random.Range(10, 80), transform.position);
+        _rigidbody.detectCollisions = false;
     }
 
 }
