7e3d67545f94911e5e60a357a01b97c052931556
gkaragoz <gkaragoz96@gmail.com>
2019-08-16 02:25:57 +0300

    Create WoodCutArea.cs script and basics.

--- Playable-Factory-DEMO/Assets/Scripts/WoodCreator.cs
+++ Playable-Factory-DEMO/Assets/Scripts/WoodCreator.cs
@@ -3,6 +3,8 @@
 public class WoodCreator : MonoBehaviour {
 
     [SerializeField]
+    private WoodCutArea _woodCutArea = null;
+    [SerializeField]
     private Wood _woodLeft = null;
     [SerializeField]
     private Wood _woodRight = null;
@@ -35,10 +37,33 @@ public class WoodCreator : MonoBehaviour {
             _woodLeft.transform.position.x + GetTotalLengthOfWood(), 
             _woodRight.transform.position.y, 
             _woodRight.transform.position.z);
+
+        // ReCreate green loop ring.
+        _woodCutArea.ReCreate();
+    }
+
+    public float GetWidth() {
+        return _woodLeft.transform.localScale.x;
     }
 
     public float GetTotalLengthOfWood() {
         return _woodLeft.GetLength() + _woodRight.GetLength();
+    }
+
+    public float GetRightCornerPosXOfWood() {
+        return _woodRight.transform.position.x;
+    }
+
+    public float GetLeftCornerPosXOfWood() {
+        return _woodLeft.transform.position.x;
+    }
+
+    public float GetCenterPosXOfWood() {
+        return (transform.position.x + GetTotalLengthOfWood()) * 0.5f;
+    }
+
+    public float GetDestructionPosX() {
+        return _woodLeft.transform.position.x + _woodLeft.GetLength();
     }
 
 }
---
+++ Playable-Factory-DEMO/Assets/Scripts/WoodCutArea.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class WoodCutArea : MonoBehaviour {
+
+    [SerializeField]
+    private WoodCreator _woodCreator;
+
+    [SerializeField]
+    private float _extraMultiplier = 1.025f;
+    [SerializeField]
+    private float _minLength = 0.15f;
+    [SerializeField]
+    private float _maxLength = 0.5f;
+
+    public bool IsCreationValid() {
+        Debug.Log("RingRight: " + GetRightCornerPosXOfTheRing());
+        Debug.Log("RingLeft: " + GetLeftCornerPosXOfTheRing());
+        Debug.Log("WoodRight: " + _woodCreator.GetRightCornerPosXOfWood());
+        Debug.Log("WoodLeft: " + _woodCreator.GetLeftCornerPosXOfWood());
+        Debug.Log("WoodCenter: " + _woodCreator.GetCenterPosXOfWood());
+        Debug.Log("WoodDesPos: " + _woodCreator.GetDestructionPosX());
+
+        if (GetRightCornerPosXOfTheRing() > _woodCreator.GetRightCornerPosXOfWood() || GetLeftCornerPosXOfTheRing() < _woodCreator.GetLeftCornerPosXOfWood()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void ReCreate() {
+        IsCreationValid();
+        do {
+            ReScale();
+            RePosition();
+        } while (!IsCreationValid());
+    }
+
+    public void RePosition() {
+        float randomPosition = Random.Range(_woodCreator.GetLeftCornerPosXOfWood(), _woodCreator.GetRightCornerPosXOfWood()) - transform.localScale.y;
+
+        transform.position = new Vector3(randomPosition, transform.position.y, transform.position.z);
+    }
+
+    public void ReScale() {
+        float randomLength = Random.Range(_minLength, _maxLength);
+
+        // Set random length and width to left wood.
+        transform.localScale = new Vector3(
+            _woodCreator.GetWidth() * _extraMultiplier,
+            Random.Range(_minLength, _maxLength),
+            _woodCreator.GetWidth() * _extraMultiplier);
+    }
+
+    public float GetRightCornerPosXOfTheRing() {
+        return transform.position.x + (transform.localScale.y * 2f);
+    }
+
+    public float GetLeftCornerPosXOfTheRing() {
+        return transform.position.x - (transform.localScale.y * 2f);
+    }
+
+}
